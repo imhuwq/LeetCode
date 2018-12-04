9825f09e82ac07d24defb68aa5c26274642a3496
imhuwq <imhuwq@gmail.com>
2018-12-04 20:40:25 +0800

    new(solution): RemoveElement

--- include/0027_RemoveElement.h
+++ include/0027_RemoveElement.h
@@ -32,17 +32,46 @@ namespace L0027 {
 
     class Solution {
     public:
-        int removeElement(vector<int>& nums, int val) {
-            
+        int removeElement(vector<int> &nums, int val) {
+            int pos = 0;
+            int end = 0;
+            while (pos < nums.size()) {
+                if (nums[pos] != val) {
+                    nums[end++] = nums[pos];
+                }
+                pos++;
+            }
+            return end;
         }
     };
-
 }
 
 TEST(L0027_REMOVE_ELEMENT, CASE_TEST) {
     L0027::Solution s;
-    
-    EXPECT_EQ(1, 1);
+
+    vector<int> nums1 = {3, 2, 2, 3};
+    int len1 = s.removeElement(nums1, 3);
+    EXPECT_EQ(len1, 2);
+
+    vector<int> nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+    int len2 = s.removeElement(nums2, 2);
+    EXPECT_EQ(len2, 5);
+
+    vector<int> nums3 = {4, 5};
+    int len3 = s.removeElement(nums3, 4);
+    EXPECT_EQ(len3, 1);
+
+    vector<int> nums4 = {1};
+    int len4 = s.removeElement(nums4, 1);
+    EXPECT_EQ(len4, 0);
+
+    vector<int> nums5 = {3, 3};
+    int len5 = s.removeElement(nums5, 3);
+    EXPECT_EQ(len5, 0);
+
+    vector<int> nums6 = {4, 5};
+    int len6 = s.removeElement(nums6, 5);
+    EXPECT_EQ(len6, 1);
 }
 
 #endif //LEET_CODE_0027_REMOVE_ELEMENT_H_
