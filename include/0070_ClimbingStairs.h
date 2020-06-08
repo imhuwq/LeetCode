370bfa4c8619a6ffad23aa21d3f9eb0b41115f89
imhuwq <imhuwq@gmail.com>
2020-06-08 22:12:36 +0800

    new(solution): ClimbingStairs

--- include/0070_ClimbingStairs.h
+++ include/0070_ClimbingStairs.h
@@ -43,7 +43,15 @@ namespace L0070 {
         int climbStairs(int n) {
             if (n == 0) return 1;
             else if (n == 1) return 1;
-            return climbStairs(n - 1) + climbStairs(n - 2);
+
+            int result, n_1 = 1, n_2 = 1;
+            for (int i = 2; i <= n; i++) {
+                result = n_1 + n_2;
+                n_2 = n_1;
+                n_1 = result;
+            }
+
+            return result;
         }
     };
 }
